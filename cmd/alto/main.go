df326963cec4280cf22334f6523d17219b8856e3
ItsLychee <itslychee@protonmail.com>
2021-12-08 22:47:52 -0600

    add configuration loading

--- cmd/alto/main.go
+++ cmd/alto/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	logging "log"
+	"encoding/json"
+	"flag"
 	"os"
+	"path/filepath"
 
 	"github.com/dhowden/tag"
 )
 
-var log = logging.New(os.Stderr, "] ", logging.Lmsgprefix|logging.LstdFlags)
 var SupportedFormats = []tag.FileType{
 	tag.MP3,
 	tag.M4A,
@@ -19,7 +20,36 @@ var SupportedFormats = []tag.FileType{
 	tag.DSF,
 }
 
+type Config struct {
+	Path string `json:"path"`
+}
+
 func main() {
-	
+	var config Config
+	var configPath Filepath
+	var destination string
+	var source string
+
+	flag.Var(configPath, "config", "custom path to configuration file")
+	flag.StringVar(&config.Path, "path", "", "how alto should rename your files")
+	flag.StringVar(&source, "source", ".", "where should alto index and read from")
+	flag.StringVar(&destination, "destination", ".", "where should alto write to")
+	flag.Parse()
+
+	// Configuration loading
+	if configPath.String() == "" {
+		confdir, _ := os.UserConfigDir()
+		defaultConfigFile := filepath.Join(confdir, "alto", "config.json")
+		buf, _ := os.ReadFile(defaultConfigFile)
+		json.Unmarshal(buf, &config)
+	} else {
+		buf, err := os.ReadFile(configPath.String())
+		if err != nil {
+			panic(err)
+		}
+		if err := json.Unmarshal(buf, &config); err != nil {
+			panic(err)
+		}
+	}
 
 }
