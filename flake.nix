6bf36ede9c8667e25357cc3385b53e2b5a495127
Lychee <itslychee@protonmail.com>
2022-06-13 08:07:29 -0500

    Add flake.nix and flake.lock
    These files are meant for development purposes and serve no purpose in
    releases. In the future I'll eventually make these more convenient for
    the developers using Nix, such as I.

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A very basic flake";
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachSystem utils.lib.allSystems (system: let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.default = pkgs.mkShell {
+      packages = with pkgs; [
+        go_1_18
+        go-tools
+        gopls
+      ];
+    };
+  });
+}
